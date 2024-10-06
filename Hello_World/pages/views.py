d3b9fd4f3437ac87ad0089c8119f350de1e59976
Fonij80 <forooz.iraji@gmail.com>
2024-10-06 21:00:13 +0330

    complete pages app

--- Hello_World/pages/views.py
+++ Hello_World/pages/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
+
+# function-based view
 def homePageView(request):
     return HttpResponse("Hello World!")
+
+# class-based view
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
