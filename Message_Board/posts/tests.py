2742544e8175a9476ca5bf9aae38d933d5859fb8
Fonij80 <forooz.iraji@gmail.com>
2024-10-07 17:29:24 +0330

    add Message_Board tests

--- Message_Board/posts/tests.py
+++ Message_Board/posts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+class PostTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="Test")
+
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "Test")
+
+    def test_url_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homePage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "Test")
+
