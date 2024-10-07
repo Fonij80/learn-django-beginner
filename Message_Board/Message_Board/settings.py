98849e917c0f04b80b9102f4146c7c9654503643
Fonij80 <forooz.iraji@gmail.com>
2024-10-07 13:52:27 +0330

    complete Message_Board App

--- Message_Board/Message_Board/settings.py
+++ Message_Board/Message_Board/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "Message_Board.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
