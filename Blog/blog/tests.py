055fbedfc993602948189f7bf4d21bb4cf2f5b9b
Fonij80 <forooz.iraji@gmail.com>
2024-10-13 20:47:18 +0330

    complete Forms in BlogApp

--- Blog/blog/tests.py
+++ Blog/blog/tests.py
@@ -1,6 +1,10 @@
+from django.db.models import DateTimeField
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.utils.dateparse import parse_datetime
+from django.utils import timezone
+
 from .models import Post
 
 
@@ -43,10 +47,42 @@ class BlogTests(TestCase):
         self.assertTemplateUsed(response, "home.html")
         self.assertContains(response, "Test Body")
 
-    def test_postDetailPage(self):
+    def test_post_detailView(self):
         response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
         no_response = self.client.get("/post/100000/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertTemplateUsed(response, "post_detail.html")
         self.assertContains(response, "Test Title")
+
+    def test_post_createView(self):
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "New title",
+                "body": "New text",
+                "author": self.user.id,
+                "date": "2024-12-12",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New title")
+        self.assertEqual(Post.objects.last().body, "New text")
+        self.assertEqual(Post.objects.last().date, timezone.make_aware(parse_datetime("2024-12-12")))
+
+
+    def test_post_updateView(self):
+        response = self.client.post(
+            reverse("post_update", args="1"),
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated text")
+
+    def test_post_deleteView(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
